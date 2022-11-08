54840d574e2313170f0ae5f54310a420de87068e
Anastasiia <shitovanap@gmail.com>
2022-11-08 23:36:00 +0300

    added new tables in migration

--- database/migrations/2022_11_02_192510_init_database.php
+++ database/migrations/2022_11_02_192510_init_database.php
@@ -36,6 +36,128 @@ return new class extends Migration
             $table->string('remember_token', 256)->nullable();
         });
 
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+        });
+
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('permissions')
+                ->cascadeOnDelete();
+        });
+
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')
+                ->references('id')
+                ->on('statuses')
+                ->cascadeOnDelete();
+            $table->date('placing_date');
+            $table->date('sale_date');
+            $table->date('receipt_date');
+        });
+
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id')->default(1);
+            $table->foreign('order_id')
+                ->references('id')
+                ->on('orders')
+                ->cascadeOnDelete();
+            $table->integer('article_number')->unique();
+            $table->string('name', 512);
+            $table->string('description', 2048);
+            $table->string('picture_link', 256)->nullable();
+            $table->double('purchase_price');
+            $table->double('sale_price');
+        });
+
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+        });
+
+        Schema::create('book_genre', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')
+                ->references('id')
+                ->on('genres')
+                ->cascadeOnDelete();
+        });
+
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name', 128);
+            $table->string('first_name', 128);
+            $table->string('patronymic', 128)->nullable();
+        });
+
+        Schema::create('book_author', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('authors')
+                ->cascadeOnDelete();
+        });
+
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country', 256);
+            $table->string('region', 256);
+            $table->string('city', 256);
+            $table->string('street', 256);
+            $table->integer('house');
+            $table->string('building', 24)->nullable();
+            $table->integer('flat');
+        });
+
+        Schema::create('book_warehouse', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')
+                ->references('id')
+                ->on('books')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('warehouse_id');
+            $table->foreign('warehouse_id')
+                ->references('id')
+                ->on('warehouses')
+                ->cascadeOnDelete();
+            $table->integer('count');
+        });
+
         Role::query()->insert([
             ['name' => 'покупатель'],
             ['name' => 'продавец'],
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -14,6 +14,7 @@
             <div class="card-body">
                 <p>{{$item}}</p>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
     </div>
