277a280007c7e2777e234b9f88788c0d5af84da6
Anastasiia <shitovanap@gmail.com>
2022-11-23 13:12:23 +0300

    no message

---
+++ resources/views/detailed-book-for-service.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container">
+        <div class="row m-3 align-items-center">
+            <div class="col-6 col-md-4 align-self-start">
+                <picture class="rounded mx-auto d-block">
+                    <img height="300" width="220" src="{{ Storage::url($book->file->path) }}">
+                </picture>
+            </div>
+            <div class="col-12 col-md-8">
+                <div class="card  mb-3">
+                        <div class="card-header border-success">
+                        <h4 class="card-title text-center">
+                            {{$book->name}}
+                        </h4>
+                    </div>
+                    <div class="card-body row">
+                        <div class="col-md-8">
+                            Автор:
+                            @foreach($book->authors as $author)
+                                <h6>
+                                    {{$author->first_name}} {{$author->last_name}} {{$author->patronymic}}
+                                </h6>
+                            @endforeach
+                            Жанры:
+                            @foreach($book->genres as $genre)
+                                <h7 class="text-info">
+                                    {{$genre->name}}
+                                </h7>
+                            @endforeach
+                        </div>
+                        <div class="col-md-2 text-center text-success align-self-center">
+                            <strong class="text-success">
+                                {{ $book->sale_price }} ₽
+                            </strong>
+                        </div>
+                    </div>
+                    <div class="card-footer bg-transparent ">
+                        <div>
+                            @php
+                                $count = \App\Basket::getItemCount(Auth::id(), $book->id)
+                            @endphp
+                            <form method="POST">
+                                @csrf
+                                <div class="input-group mb-3 col-md-8 offset-2">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">В корзине</span>
+                                    </div>
+                                    <input type="number" name="count" min="0" value="{{$count}}" class="form-control" />
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="submit">Сохранить</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row m-3 align-items-center">
+            <div class="card-body">
+                <h5 class="card-title text-center">
+                    Описание
+                </h5>
+                <p class="card-text">
+                    {{$book->description}}
+                </p>
+            </div>
+        </div>
+    </div>
+@stop
